c59b1430a6459a910689f1113b2ca1ddeedc25d9
Arianit Uka <arianitu@gmail.com>
2016-01-31 20:56:55 -0500

    Account orders and activities

--- accounts.go
+++ accounts.go
@@ -1,6 +1,7 @@
 package questrade
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"time"
@@ -22,7 +23,7 @@ type Accounts struct {
 
 type Position struct {
 	Symbol             string
-	SymbolId           int
+	SymbolId           uint64
 	OpenQuantity       float64
 	ClosedQuantity     float64
 	CurrentMarketValue float64
@@ -58,7 +59,7 @@ type Balances struct {
 
 type Execution struct {
 	Symbol                   string
-	SymbolId                 int
+	SymbolId                 uint64
 	Quantity                 int
 	Side                     string
 	Price                    float64
@@ -81,6 +82,31 @@ type Execution struct {
 type Executions struct {
 	Executions []Execution
 }
+
+type Activities struct {
+	Activities []Activity
+}
+
+type Activity struct {
+	TradeDate       string
+	TransactionDate string
+	SettlementDate  string
+	Action          string
+	Symbol          string
+	SymbolId        uint64
+	Description     string
+	Currency        string
+	Quantity        float64
+	Price           float64
+	GrossAmount     float64
+	Commission      float64
+	NetAmount       float64
+	Type            string
+}
+
+var (
+	OrderNotFound = errors.New("Order not found")
+)
 
 func (c *Client) Accounts() ([]Account, error) {
 	var accounts Accounts
@@ -117,11 +143,11 @@ func (c *Client) Balances(account Account) (*Balances, error) {
 }
 
 func (c *Client) Executions(account Account, startTime time.Time, endTime time.Time) (*Executions, error) {
-	v := url.Values{}
-	v.Add("startTime", startTime.Format(time.RFC3339))
-	v.Add("endTime", endTime.Format(time.RFC3339))
+	parameters := url.Values{}
+	parameters.Add("startTime", startTime.Format(time.RFC3339))
+	parameters.Add("endTime", endTime.Format(time.RFC3339))
 
-	u := fmt.Sprintf("accounts/%v/executions?%v", account.Number, v.Encode())
+	u := fmt.Sprintf("accounts/%vp/executions?%v", account.Number, parameters.Encode())
 	fmt.Println(u)
 
 	var executions Executions
@@ -131,4 +157,61 @@ func (c *Client) Executions(account Account, startTime time.Time, endTime time.T
 	}
 
 	return &executions, nil
+}
+
+func (c *Client) getOrders(account Account, parameters url.Values) (*Orders, error) {
+	u := fmt.Sprintf("accounts/%vp/orders?%v", account.Number, parameters.Encode())
+	fmt.Println(u)
+
+	var orders Orders
+	err := c.NewRequest("GET", u, nil, &orders)
+	if err != nil {
+		return nil, err
+	}
+
+	return &orders, nil
+}
+
+func (c *Client) OrderById(account Account, orderId int, stateFilter string) (*Order, error) {
+	parameters := url.Values{}
+	parameters.Add("orderId", string(orderId))
+	parameters.Add("stateFilter", stateFilter)
+
+	orders, err := c.getOrders(account, parameters)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(orders.Orders) == 0 {
+		return nil, OrderNotFound
+	}
+
+	return &orders.Orders[0], nil
+}
+
+func (c *Client) Orders(account Account, startTime time.Time, endTime time.Time, stateFilter string) (*Orders, error) {
+	parameters := url.Values{}
+	parameters.Add("startTime", startTime.Format(time.RFC3339))
+	parameters.Add("endTime", endTime.Format(time.RFC3339))
+	parameters.Add("stateFilter", stateFilter)
+
+	return c.getOrders(account, parameters)
+}
+
+func (c *Client) Activities(account Account, startTime time.Time, endTime time.Time) (*Activities, error) {
+	parameters := url.Values{}
+	parameters.Add("startTime", startTime.Format(time.RFC3339))
+	parameters.Add("endTime", endTime.Format(time.RFC3339))
+
+	u := fmt.Sprintf("accounts/%vp/activities?%v", account.Number, parameters.Encode())
+	fmt.Println(u)
+
+	var activities Activities
+	err := c.NewRequest("GET", u, nil, &activities)
+	if err != nil {
+		return nil, err
+	}
+
+	return &activities, nil
+
 }
--- orders.go
+++ orders.go
@@ -1,1 +1,53 @@
 package questrade
+
+type OrderLeg struct {
+}
+
+type Order struct {
+	Id                       int
+	Symbol                   string
+	SymbolId                 int
+	TotalQuantity            int
+	OpenQuantity             int
+	FilledQuantity           int
+	CanceledQuantity         int
+	Side                     string
+	OrderType                string
+	LimitPrice               int
+	StopPrice                float64
+	IsAllOrNone              bool
+	IsAnonymous              bool
+	IcebergQuantity          int
+	MinQuantity              int
+	AvgExecPrice             float64
+	LastExecPrice            float64
+	Source                   string
+	TimeInForce              string
+	GtdDate                  string
+	State                    string
+	ClientReasonStr          string
+	ChainId                  int
+	CreationTime             string
+	UpdateTime               string
+	Notes                    string
+	PrimaryRoute             string
+	SecondaryRoute           string
+	OrderRoute               string
+	VenueHoldingOrder        string
+	CommissionCharged        float64
+	ExchangeOrderId          string
+	IsSignificantShareholder bool
+	IsInsider                bool
+	IsLimitOffsetInDollar    bool
+	UserId                   int
+	PlacementCommission      float64
+	Legs                     []OrderLeg
+	StrategyType             string
+	TriggerStopPrice         float64
+	OrderGroupId             int
+	OrderClass               string
+}
+
+type Orders struct {
+	Orders []Order
+}
